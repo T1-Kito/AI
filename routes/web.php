6dd696f9ea33ffb6a331c8a3e64c0d51c1e59a83
danghoanthang <hoanthang0307@gmail.com>
2026-05-03 19:43:14 +0700

    update1
    update1

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login.store');
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register'])->name('register.store');
+    Route::get('/auth/google/redirect', [AuthController::class, 'redirectToGoogle'])->name('auth.google.redirect');
+    Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('auth.google.callback');
 });
 
 Route::get('/guides/cursor-pro', function () {
